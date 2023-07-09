3445f6ea3a03e91f681f1a49db2b901eb7ce7454
Brian Yang <anan0329@yahoo.com.tw>
2023-07-10 00:07:29 +0800

    fix r-packages-install.sh typo

--- codeserver-python-r/r-packages-install.sh
+++ codeserver-python-r/r-packages-install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-cat /tmp/r-packages.txt | while read extension || [[ -n $package ]];
+cat /tmp/r-packages.txt | while read package || [[ -n $package ]];
 do
-  RUN R -e "install.packages('$package', dependencies=TRUE, repos='http://cran.rstudio.com/')"
-#   code-server --install-extension $extension
+  R -e "install.packages('$package', dependencies=TRUE, repos='http://cran.rstudio.com/')"
 done
